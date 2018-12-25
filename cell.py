a3aff3b021e5080efdb449d0761b309ed96a8c6c
Sarthak Rijal <rijals@uw.edu>
2018-12-25 01:07:20 -0500

    Functional but not working properly

--- cell.py
+++ cell.py
@@ -1,11 +1,13 @@
 import pygame
 import sys
+import random
 pygame.init()
 
 # cell class
 class cell(object): 
     # Function to initialise the node object 
-    def __init__(self, screen, DOA, x, y, coor_x, coor_y, size = 40, color = (0,0,0), margin = 5):
+    def __init__(self, screen, DOA, x, y, coor_x, coor_y, pressed = False, size = 40,
+                                                 color = (0,0,0), margin = 5, neighbours = []):
         self.margin = margin
         self.x = x
         self.y = y
@@ -16,52 +18,83 @@ class cell(object):
         self.size = size 
         self.rect = pygame.Rect(self.coor_x, self.coor_y, self.size, self.size)
         self.DOA = DOA  # Assign data 
-        self.N = None
-        self.E = None
-        self.S = None
-        self.W = None
-        self.NE = None
-        self.SE = None
-        self.NW = None
-        self.SW = None
+        self.pressed = pressed
+        self.neighbours = neighbours
 
-        if (self.DOA):
-            self.color = (0,0,0)
-
-
-
-
+    #detects the mouse hovering, logic of clicking a cell
     def detect_mouse(self, x, y):
         if (self.rect.collidepoint(x,y)):
-            self.alive()
-            if (pygame.mouse.get_pressed() == (1,0,0)):
-                self.alive()
-            elif(pygame.mouse.get_pressed() == (0,0,1)):
-                print(pygame.mouse.get_pressed())
+            self.potentiallyAlive()
+            if (pygame.mouse.get_pressed()[0]):
+                self.pressedAlive()
+            elif (pygame.mouse.get_pressed()[2]):
                 self.dead()
-        else: 
-            self.dead()
+        else:
+            if(not self.pressed):
+                self.dead()
 
-
+    #makes the cell dead 
     def dead(self):
+        self.pressed = False
         self.DOA = False
         self.color = (192,192,192)
 
-    def alive(self):
+    #makes the cell alive
+    def potentiallyAlive(self):
+        self.color = (0,0,0)
+
+    #makes the cell alive when clicked
+    def pressedAlive(self):
+        self.pressed = True
         self.DOA = True
         self.color = (0,0,0)
 
-
-            
-
-        
     
-    def _draw(self, screen):
+    def addNeigbour(self, neighbour):
+        self.neighbours = neighbour
+        
+    #draws the cell
+    def _draw (self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
                                                   
 
+    def countAliveNeighbour (self):
+        neighbours_alive = 0
+        for i in self.neighbours:
+            if (i.DOA == True):
+                neighbours_alive += 1
+        return neighbours_alive
+
+    def toString(self):
+        return str(self.x)+","+str(self.y)
+
+    def update(self):
+        alive_near = self.countAliveNeighbour()
+        if (self.DOA == False):
+            if (alive_near == 3):
+                self.pressedAlive()
+        if (self.DOA == True):
+            if (alive_near == 2 and alive_near == 3):
+                self.pressedAlive()            
+            if (alive_near == 0 or alive_near == 1):
+                self.dead()
+            if (alive_near > 3):
+                self.dead()
+
+            
+            
 
 
-   # def countNeighbout(self): nee to count neighbours
+
+    #rules of the game
+
+    # 1) Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    # 2) Any live cell with two or three live neighbors lives on to the next generation.
+    # 3) Any live cell with more than three live neighbors dies, as if by overpopulation.
+    # 4) Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+        
+
+   #def countNeighbout(self): need to count neighbours
        
         
--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import math
 pygame.init()
 
 #screen size and setup
-size = (2255, 1405)
+size = (2255, 1360)
 screen = pygame.display.set_mode(size)
 grid_color = (192,192,192)
 #FPS
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 200
 
 row = 30#rows
 col = 50#columns
@@ -23,6 +23,9 @@ col = 50#columns
 #GLOBAL CELL ATTRIBUTES
 margin = 5
 CELL_SIZE = 40
+
+pressed = False
+
 
 # MAKE THE GRID
 #hardcoded number of grids and 
@@ -34,12 +37,68 @@ for i in range(col):
                                                 margin + (CELL_SIZE + margin)*j, 
                                                 color = grid_color ))
     GRID.append(column)
+
+
+
+#can i do this in a better way?
+for i in range(col):
+    for j in range(row):
+        #connected the corners
+        #top left
+        if (i == 0 and j == 0):            
+            GRID[i][j].addNeigbour([GRID[i+1][j], GRID[i+1][j+1], GRID[i][j+1]])
+            
+        #bottom left
+        elif (i == 0 and j == row - 1):
+            GRID[i][j].addNeigbour([GRID[i+1][j], GRID[i+1][j-1], GRID[i][j-1]])
+            
+        #bottom right
+        elif (i == col - 1 and j == row - 1):
+            GRID[i][j].addNeigbour([GRID[i-1][j], GRID[i-1][j-1], GRID[i][j-1]])
+        #top right
+        elif (i == col - 1 and j == 0 ):
+            GRID[i][j].addNeigbour([GRID[i-1][j], GRID[i-1][j+1], GRID[i][j-1]])
+
+
+        #connecting the edges - the corners
+        #left edge not including topLeft and bottomLeft corners
+        elif (i == 0 and not ((i == 0 and j == 0) or (i == 0 and j == row - 1))):
+            GRID[i][j].addNeigbour([GRID[i][j-1], GRID[i+1][j-1], GRID[i+1][j], GRID[i+1][j+1]])#, GRID[i][j+1]])
+        #right edge not including topRight and Bottom right
+        elif (i == col - 1 and not ((i == col - 1 and j == 0) or (i == col - 1 and j == row - 1))):
+            GRID[i][j].addNeigbour([GRID[i][j-1], GRID[i-1][j-1], GRID[i-1][j], GRID[i-1][j+1], GRID[i][j+1]])
+        #top edge not including topLeft and topRight
+        elif (j == 0 and not ((i == 0 and j == 0) or (i == col - 1 and j == 0))):
+            GRID[i][j].addNeigbour([GRID[i+1][j], GRID[i+1][j+1], GRID[i][j+1], GRID[i-1][j+1], GRID[i-1][j]])
+        #bottom edge
+        elif (j == row - 1 and not ((i == 0 and j == row - 1) or (i == col - 1 and j == row - 1))):
+            GRID[i][j].addNeigbour([GRID[i+1][j], GRID[i+1][j-1], GRID[i][j-1], GRID[i-1][j-1], GRID[i-1][j]])
+
+        #every thing else 
+        else:
+            GRID[i][j].addNeigbour([ GRID[i+1][j], GRID[i+1][j-1], GRID[i][j-1], GRID[i-1][j-1], 
+                                     GRID[i-1][j], GRID[i+1][j+1], GRID[i][j+1], GRID[i-1][j+1] ])
+
+
+for i in range(col):
+            for j in range(row):
+                GRID[i][j]._draw(screen)
+                
+
+
+def print_f(msg,siz,color,x,y):
+    Text = pygame.font.Font("bit5x3.ttf",siz)
+    write = Text.render(msg,True,color)
+    write_rect = write.get_rect()
+    write_rect.center = ((x),(y))
+    screen.blit(write,write_rect)
+
     
 
-
-
-
-while True:
+def play(GRID, pressed):
+    while True:
+        
+        x,y = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -51,27 +110,45 @@ while True:
                 else:
                     pygame.display.set_mode(size, pygame.FULLSCREEN)
 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                GRID = []
+                for i in range(col):
+                    column = []
+                    for j in range(row):
+                        column.append(cell(screen, False, i, j, margin + (CELL_SIZE + margin)*i, 
+                                                                margin + (CELL_SIZE + margin)*j, 
+                                                                color = grid_color ))
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                pressed = not pressed
+
+        # for i in range(col):
+        #     for j in range(row):
+        #         GRID[i][j]._draw(screen)
+        #         GRID[i][j].detect_mouse(x,y) 
+        #         l = str(GRID[i][j].x)
+        #         k = str(GRID[i][j].y)
+        #         print_f(l+","+k, 20, (0,0,0), GRID[i][j].coor_x+20,  GRID[i][j].coor_y+20)
+
+        if pressed:
+            for j in range(row):
+                for i in range(col):
+                    GRID[i][j]._draw(screen)
+                    GRID[i][j].update()
+        else:
+            for j in range(row):
+                for i in range(col):
+                    GRID[i][j]._draw(screen)
+                    GRID[i][j].detect_mouse(x, y)
         
-        x,y = pygame.mouse.get_pos()        
-        #check for mouse hover and
-        #HASH function to store cell with its coordinates
-        for j in range(row):
-            for i in range(col):
-                GRID[i][j]._draw(screen)
-                GRID[i][j].detect_mouse(x,y)    
-
-        mouse_pos = pygame.mouse.get_pos()
-
-    
-                
-           
-
-                
-
-    # pygame.draw.rect(screen, (255,255,255),[40+margin+margin, 0+margin+margin, 40,40] )
-
+       #DEBUGGING COORDINATES
+        coorx = 20
+        coory = 20
+        
+        print_f("THIS", 20, (0,0,0), GRID[coorx][coory].coor_x+20,  GRID[coorx][coory].coor_y+20)
+        print(GRID[coorx][coory].countAliveNeighbour())
 
         pygame.display.update()
         clock.tick(FPS)
 
-			
+play(GRID, pressed)
